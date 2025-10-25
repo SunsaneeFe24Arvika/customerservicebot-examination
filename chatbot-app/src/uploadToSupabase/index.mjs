5a01e3266149e03b6ca2bf9663394088dd70538d
Sunsanee Persson <3kdyhfe24.perssu@folkuniversitetet.nu>
2025-10-25 14:56:36 +0200

    Skapat supabase dokument

---
+++ chatbot-app/src/uploadToSupabase/index.mjs
@@ -0,0 +1,47 @@
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { OllamaEmbeddings } from '@langchain/ollama';
+import { createClient } from '@supabase/supabase-js';
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { readFile } from 'fs/promises';
+import "dotenv/config";
+
+
+const combineDocuments = (docs) => {
+    return docs.map(doc => doc.pageContent).join('\n\n');
+}
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
+
+const file = '';
+
+try {
+    const text = await readFile(`${process.cwd()}/info.txt`, "utf-8");
+
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize : 500,
+        separators : ["\n\n", "\n", " ", ""],
+        chunkOverlap : 50
+    });
+
+
+    const splittedText = await textSplitter.createDocuments([text]);
+
+    const supabaseClient = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+
+    await SupabaseVectorStore.fromDocuments(
+        splittedText,
+        new OllamaEmbeddings({
+            model : "nomic-embed-text:latest"
+        }), {
+            client : supabaseClient,
+            tableName : 'documents'
+        }
+    );
+
+    console.log('Success');
+    
+} catch(error) {
+    console.log('ERROR:', error.message);
+    
+}
